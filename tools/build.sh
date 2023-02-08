0d948ae9419a0802e016fa0478501b971345c7f7
Anshuman Goel <ansgoel@microsoft.com>
2023-02-08 13:08:35 -0800

    to set 2.7.0 path

--- tools/build.sh
+++ tools/build.sh
@@ -9,7 +9,7 @@
 set -eu
 
 cat ./.bundle/config
-export PATH=/home/runner/work/edgevr/edgevr/vendor/bundle/ruby/2.7.0/bin:/home/runner/work/edgevr/edgevr/vendor/bundle/ruby/2.7.0/gems:$PATH
+export PATH=/home/runner/work/edgevr/edgevr/vendor/bundle/ruby/2.7.0:$PATH
 
 CMD="JEKYLL_ENV=production bundle exec jekyll b"
 
